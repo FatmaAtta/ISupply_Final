fd55cb7b779b1bc673506f4dce3f1b4df68367bc
FatmaAtta <fatmamaali@gmail.com>
2025-06-29 00:15:15 +0300

    modified seller's screen UI

--- lib/seller.dart
+++ lib/seller.dart
@@ -3,6 +3,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SellerScreen extends StatelessWidget {
   final String sellerID;
+  Map<int, String> status ={
+    0: "Pending",
+    1: "Confirmed",
+    2: "Shipping",
+    3: "Delivered",
+  };
+  Map<int, String> nextStatus ={
+    0: "Pending",
+    1: "Confirm",
+    2: "Ship",
+    3: "Deliver",
+  };
 
   SellerScreen({required this.sellerID});
 
@@ -16,7 +28,7 @@ class SellerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Seller Orders")),
+      appBar: AppBar(title: Text("${sellerID}'s Orders")),
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance
             .collection('Orders')
@@ -33,14 +45,30 @@ class SellerScreen extends StatelessWidget {
               final order = orders[index].data() as Map<String, dynamic>;
               return ListTile(
                 title: Text("Order ${orders[index].id}"),
-                subtitle: Text("Status: ${order['status']}"),
-                trailing: ElevatedButton(
-                  onPressed: () {
-                    int newStatus = (order['status'] ?? 0) + 1;
-                    updateOrderStatus(orders[index].id, newStatus);
-                  },
-                  child: Text("Next Status"),
-                ),
+                subtitle: Text("Status: ${status[order['status']]}"),
+                trailing: (order['status']>=3) ?
+                Text(
+                  "Delivered",
+                   style: TextStyle(color: Color(0xff79c7c0)),
+                ) :
+                    SizedBox(
+                      width: 150,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          int newStatus = (order['status'] ?? 0) + 1;
+                          updateOrderStatus(orders[index].id, newStatus);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xff2bd1ff), // Button background
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8), // Decrease for less curve
+                          ),
+                          // Text/icon color
+                        ),
+                        child: Text("${nextStatus[order['status']+1]}"),
+                      ),
+                    )
               );
             },
           );
